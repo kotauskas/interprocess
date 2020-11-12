3a677dd816111b5a1854e126c5bae2dc3ff07646
Kotauskas <v.toncharov@gmail.com>
2020-11-12 19:35:55 +0300

    Adjusted readme to remove Tokio mention

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,8 @@
 //! - **FIFO files** — Unix-specific type of file which is similar to unnamed pipes but exists on the filesystem, often referred to as "named pipes" but completely different from Windows named pipes
 //! - **Unix domain sockets** — Unix-specific socket type which is extremely similar to normal network sockets but uses filesystem paths instead, with the optional Linux feature allowing them to use a spearate namespace akin to Windows named pipes
 //! - **Windows named pipes** — Windows-specific named pipe interface closely resembling Unix domain sockets
-//! - **Local sockets** — platform independent interface utilizing named pipes on Windows and Unix domain sockets on Unix
+//! - **Local sockets** — platform independent interface utilizing named pipes on Windows and Unix domain sockets on Unix. **Async support included!**
 //! - **Signals** — Unix-specific signals, used to receive critical messages from the OS and other programs, as well as sending those messages
-//!
-//! ## Planned
-//! - **Tokio integration** — usage of `spawn_blocking` for the purposes of creating async versions of blocking pipe APIs
 //!
 //! # License
 //! This crate, along with all community contributions made to it, is dual-licensed under the terms of either the [MIT license] or the [Apache 2.0 license].
