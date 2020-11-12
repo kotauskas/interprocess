6f3b2764fc3b057675e3bbd9de111e732142b39f
Kotauskas <v.toncharov@gmail.com>
2020-11-12 19:21:52 +0300

    Added documentation for the async wrapper

--- src/nonblocking/local_socket.rs
+++ src/nonblocking/local_socket.rs
@@ -1,3 +1,9 @@
+//! Asynchronous local sockets.
+//!
+//! See the [blocking version of this module] for more on what those are.
+//!
+//! [blocking version of this module]: ../../local_socket/index.html " "
+
 use blocking::{unblock, Unblock};
 use futures::{
     stream::{unfold, Stream},
@@ -10,33 +16,65 @@ use std::{
 
 use crate::local_socket::{self as sync, ToLocalSocketName};
 
-#[derive(Clone)]
+/// An asynchronous local socket server, listening for connections.
+///
+/// # Example
+/// ```no_run
+/// use futures::{
+///     io::{BufReader, AsyncBufReadExt, AsyncWriteExt},
+///     stream::TryStreamExt,
+/// };
+/// use interprocess::nonblocking::local_socket::*;
+///
+/// let listener = LocalSocketListener::bind("/tmp/example.sock")
+///     .await?;
+/// listener
+///     .incoming()
+///     .try_for_each(|mut conn| async move {
+///         conn.write_all(b"Hello from server!\n").await?;
+///         let mut conn = BufReader::new(conn);
+///         let mut buffer = String::new();
+///         conn.read_line(&mut buffer).await?;
+///         println!("Client answered: {}", buffer);
+///         Ok(())
+///     })
+///     .await?;
+/// # Ok::<(), Box<dyn std::error::Error>>(())
+/// ```
+#[derive(Debug)]
 pub struct LocalSocketListener {
     inner: Arc<sync::LocalSocketListener>,
 }
 
 impl LocalSocketListener {
-    pub async fn bind<'a, T>(name: T) -> io::Result<Self>
-    where
-        T: ToLocalSocketName<'a> + Send + 'static,
-    {
+    /// Creates a socket server with the specified local socket name.
+    #[inline]
+    pub async fn bind<'a>(name: impl ToLocalSocketName<'_> + Send + 'static) -> io::Result<Self> {
         Ok(Self {
             inner: Arc::new(unblock(move || sync::LocalSocketListener::bind(name)).await?),
         })
     }
+    /// Listens for incoming connections to the socket, blocking until a client is connected.
+    ///
+    /// See [`incoming`] for a convenient way to create a main loop for a server.
+    ///
+    /// [`incoming`]: #method.incoming " "
+    #[inline]
     pub async fn accept(&self) -> io::Result<LocalSocketStream> {
         let s = self.inner.clone();
         Ok(LocalSocketStream {
             inner: Unblock::new(unblock(move || s.accept()).await?),
         })
     }
+    /// Creates an infinite iterator which calls `accept()` with each iteration. Used together with `for` loops to conveniently create a main loop for a socket server.
+    #[inline]
     pub fn incoming(&self) -> impl Stream<Item = std::io::Result<LocalSocketStream>> {
         // TODO (?) : effectively `clone`s the Arc twice for every Item.
         //      This could be fixed by copying the code from the `accept` function
         //      or creating an alernative `accept` function that takes `self`
-        let s = self.clone();
+        let s = self.inner.clone();
         unfold((), move |()| {
-            let s = s.inner.clone();
+            let s = Arc::clone(&s);
             async move {
                 Some((
                     unblock(move || s.accept())
@@ -51,14 +89,33 @@ impl LocalSocketListener {
     }
 }
 
+/// An asynchronous local socket byte stream, obtained eiter from [`LocalSocketListener`] or by connecting to an existing local socket.
+///
+/// # Example
+/// ```no_run
+/// use futures::io::{BufReader, AsyncBufReadExt, AsyncWriteExt};
+/// use interprocess::nonblocking::local_socket::*;
+///
+/// // Replace the path as necessary on Windows.
+/// let mut conn = LocalSocketStream::connect("/tmp/example.sock")
+///     .await?;
+/// conn.write_all(b"Hello from client!\n").await?;
+/// let mut conn = BufReader::new(conn);
+/// let mut buffer = String::new();
+/// conn.read_line(&mut buffer).await?;
+/// println!("Server answered: {}", buffer);
+/// # Ok::<(), Box<dyn std::error::Error>>(())
+/// ```
+///
+/// [`LocalSocketListener`]: struct.LocalSocketListener.html " "
 pub struct LocalSocketStream {
     inner: Unblock<sync::LocalSocketStream>,
 }
 impl LocalSocketStream {
-    pub async fn connect<'a, T>(name: T) -> io::Result<Self>
-    where
-        T: ToLocalSocketName<'a> + Send + 'static,
-    {
+    /// Connects to a remote local socket server.
+    pub async fn connect<'a>(
+        name: impl ToLocalSocketName<'a> + Send + 'static,
+    ) -> io::Result<Self> {
         Ok(Self {
             inner: Unblock::new(unblock(move || sync::LocalSocketStream::connect(name)).await?),
         })
