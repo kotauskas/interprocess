d1d8af26f8225822842aa4d5e5bf25ab3b6ae339
Kotauskas <v.toncharov@gmail.com>
2021-07-31 13:28:21 +0300

    Readme revamp for the occasion

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 //! Interprocess communication toolkit for Rust programs. The crate aims to expose as many platform-specific features as possible while maintaining a uniform interface for all platforms.
 //!
 //! # Features
-//! The following interprocess communication primitives are implemented:
-//! - **Unnamed pipes** — anonymous file-like objects for communicating privately in one direction, most commonly used to communicate between a child process and its parent
-//! - **FIFO files** — Unix-specific type of file which is similar to unnamed pipes but exists on the filesystem, often referred to as "named pipes" but completely different from Windows named pipes
-//! - **Unix domain sockets** — Unix-specific socket type which is extremely similar to normal network sockets but uses filesystem paths instead, with the optional Linux feature allowing them to use a spearate namespace akin to Windows named pipes
-//! - **Windows named pipes** — Windows-specific named pipe interface closely resembling Unix domain sockets
-//! - **Local sockets** — platform independent interface utilizing named pipes on Windows and Unix domain sockets on Unix. **Async support included!**
-//! - **Signals** — Unix-specific signals, used to receive critical messages from the OS and other programs, as well as sending those messages
+//! - Cross-platform interprocess communication primitives:
+//!     - **Unnamed pipes** — anonymous file-like objects for communicating privately in one direction, most commonly used to communicate between a child process and its parent
+//!     - **Local sockets** — similar to TCP sockets, but use filesystem or namespaced paths instead of ports on `localhost`, depending on the OS, bypassing the network stack entirely; implemented using named pipes on Windows and Unix domain sockets on Unix
+//! - POSIX-specific interprocess communication primitives:
+//!     - **FIFO files** — special type of file which is similar to unnamed pipes but exists on the filesystem, often referred to as "named pipes" but completely different from Windows named pipes
+//!     - **Unix domain sockets** — a type of socket which is built around the standard networking APIs but uses filesystem paths instead of ports on `localhost`, optionally using a spearate namespace on Linux akin to Windows named pipes
+//!     - **POSIX signals** — used to receive short urgent messages from the OS and other programs, as well as sending those messages *(practical usage, other than for compatibility reasons, is strongly discouraged)*
+//! - Windows-specific interprocess communication primitives:
+//!     - **Named pipes** — closely resembles Unix domain sockets, uses a separate namespace instead of on-drive paths
+//!     - **C signals** — like POSIX signals, but with less signal types and a smaller API *(practical usage, other than for compatibility reasons, is strongly discouraged)*
+//! - **Async support** — efficient wrapper around local sockets, Windows named pipes and Ud-sockets for high-performance parallelism, currently only supports the Tokio runtime
 //!
 //! # License
 //! This crate, along with all community contributions made to it, is dual-licensed under the terms of either the [MIT license] or the [Apache 2.0 license].
