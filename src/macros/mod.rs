b8df144000fc9c24e2763c7fccb2d937afc21019
Kotauskas <v.toncharov@gmail.com>
2022-09-14 18:00:09 +0300

    Tweak `impmod` macro to work with submodules

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -4,10 +4,10 @@ mod handle_and_fd;
 mod import_tables;
 
 macro_rules! impmod {
-    ($osmod:ident, $($orig:ident $(as $into:ident)?),* $(,)?) => {
+    ($($osmod:ident)::+, $($orig:ident $(as $into:ident)?),* $(,)?) => {
         #[cfg(unix)]
-        use $crate::os::unix::$osmod::{$($orig $(as $into)?,)*};
+        use $crate::os::unix::$($osmod)::+::{$($orig $(as $into)?,)*};
         #[cfg(windows)]
-        use $crate::os::windows::$osmod::{$($orig $(as $into)?,)*};
+        use $crate::os::windows::$($osmod)::+::{$($orig $(as $into)?,)*};
     };
 }
