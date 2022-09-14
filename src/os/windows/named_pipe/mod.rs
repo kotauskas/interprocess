5038aad8cf4a252c0fda173717b522c8f460b4dd
Kotauskas <v.toncharov@gmail.com>
2022-09-14 17:59:45 +0300

    `doc(cfg)` and the pain in the ass of a thousand years

--- src/os/windows/named_pipe/mod.rs
+++ src/os/windows/named_pipe/mod.rs
@@ -27,6 +27,7 @@ pub use stream::*;
 
 #[cfg(any(doc, feature = "tokio_support"))]
 #[cfg_attr(feature = "doc_cfg", doc(cfg(feature = "tokio_support")))]
+#[cfg_attr(not(feature = "tokio_support"), allow(unused_imports))]
 pub mod tokio;
 
 use super::imports::*;
