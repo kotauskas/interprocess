6769e7a9ae86f019c5507f28d55aa1d4df0dc8a1
Kotauskas <v.toncharov@gmail.com>
2022-11-05 16:13:02 +0300

    Add reverse direction unidir Tokio NP tests

--- tests/tokio_named_pipe/main.rs
+++ tests/tokio_named_pipe/main.rs
@@ -3,9 +3,11 @@
 mod util;
 
 mod bytes;
-mod bytes_unidir;
+mod bytes_unidir_client_to_server;
+mod bytes_unidir_server_to_client;
 mod msg;
-mod msg_unidir;
+mod msg_unidir_client_to_server;
+mod msg_unidir_server_to_client;
 
 use util::TestResult;
 
@@ -15,8 +17,20 @@ async fn tokio_named_pipe_bytes() -> TestResult {
 }
 
 #[tokio::test]
-async fn tokio_named_pipe_bytes_unidir() -> TestResult {
-    util::tokio::drive_server_and_multiple_clients(bytes_unidir::server, bytes_unidir::client).await
+async fn tokio_named_pipe_bytes_unidir_client_to_server() -> TestResult {
+    util::tokio::drive_server_and_multiple_clients(
+        bytes_unidir_client_to_server::server,
+        bytes_unidir_client_to_server::client,
+    )
+    .await
+}
+#[tokio::test]
+async fn tokio_named_pipe_bytes_unidir_server_to_client() -> TestResult {
+    util::tokio::drive_server_and_multiple_clients(
+        bytes_unidir_server_to_client::server,
+        bytes_unidir_server_to_client::client,
+    )
+    .await
 }
 
 #[tokio::test]
@@ -25,6 +39,18 @@ async fn tokio_named_pipe_msg() -> TestResult {
 }
 
 #[tokio::test]
-async fn tokio_named_pipe_msg_unidir() -> TestResult {
-    util::tokio::drive_server_and_multiple_clients(msg_unidir::server, msg_unidir::client).await
+async fn tokio_named_pipe_msg_unidir_client_to_server() -> TestResult {
+    util::tokio::drive_server_and_multiple_clients(
+        msg_unidir_client_to_server::server,
+        msg_unidir_client_to_server::client,
+    )
+    .await
+}
+#[tokio::test]
+async fn tokio_named_pipe_msg_unidir_server_to_client() -> TestResult {
+    util::tokio::drive_server_and_multiple_clients(
+        msg_unidir_server_to_client::server,
+        msg_unidir_server_to_client::client,
+    )
+    .await
 }
