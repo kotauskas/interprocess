3ace73c9b5064b8b1caacdd0f5da533748f0c198
Kotauskas <v.toncharov@gmail.com>
2023-01-07 11:14:28 +0300

    Transfer local socket examples to documentation

--- src/local_socket/listener.rs
+++ src/local_socket/listener.rs
@@ -14,7 +14,99 @@ impmod! {local_socket,
 /// A local socket server, listening for connections.
 ///
 /// # Examples
-/// - [Basic server](https://github.com/kotauskas/interprocess/blob/main/examples/local_socket/server.rs)
+/// Basic server:
+/// ```no_run
+/// use interprocess::local_socket::{LocalSocketListener, LocalSocketStream, NameTypeSupport};
+/// use std::{
+///     io::{self, prelude::*, BufReader},
+///     sync::mpsc::Sender,
+/// };
+///
+/// // Define a function that checks for errors in incoming connections. We'll use this to filter
+/// // through connections that fail on initialization for one reason or another.
+/// fn handle_error(conn: io::Result<LocalSocketStream>) -> Option<LocalSocketStream> {
+///     match conn {
+///         Ok(c) => Some(c),
+///         Err(e) => {
+///             eprintln!("Incoming connection failed: {e}");
+///             None
+///         }
+///     }
+/// }
+//// // Pick a name. There isn't a helper function for this, mostly because it's largely unnecessary:
+/// // in Rust, `match` is your concise, readable and expressive decision making construct.
+/// let name = {
+///     // This scoping trick allows us to nicely contain the import inside the `match`, so that if
+///     // any imports of variants named `Both` happen down the line, they won't collide with the
+///     // enum we're working with here. Maybe someone should make a macro for this.
+///     use NameTypeSupport::*;
+///     match NameTypeSupport::query() {
+///         OnlyPaths => "/tmp/example.sock",
+///         OnlyNamespaced | Both => "@example.sock",
+///     }
+/// };
+///
+/// // Bind our listener.
+/// let listener = match LocalSocketListener::bind(name) {
+///     Err(e) if e.kind() == io::ErrorKind::AddrInUse => {
+///         // One important problem that is easy to handle improperly (or not at all) is the
+///         // "corpse sockets" that are left when a program that uses a file-type socket name
+///         // terminates its socket server without deleting the file. There's no single strategy
+///         // for handling this kind of address-already-occupied error. Services that are supposed
+///         // to only exist as a single instance running on a system should check if another
+///         // instance is actually running, and if not, delete the socket file. In this example,
+///         // we leave this up to the user, but in a real application, you usually don't want to do
+///         // that.
+///         eprintln!(
+///             "\
+///Error: could not start server because the socket file is occupied. Please check if {name} is in \
+///use by another process and try again."
+///         );
+///         return Err(e.into());
+///     }
+///     x => x?,
+/// };
+///
+/// println!("Server running at {name}");
+/// // Stand-in for the syncronization used, if any, between the client and the server.
+/// let _ = notify.send(());
+///
+/// // Preemptively allocate a sizeable buffer for reading at a later moment. This size should be
+/// // enough and should be easy to find for the allocator. Since we only have one concurrent
+/// // client, there's no need to reallocate the buffer repeatedly.
+/// let mut buffer = String::with_capacity(128);
+///
+/// for conn in listener.incoming().filter_map(handle_error) {
+///     // Wrap the connection into a buffered reader right away
+///     // so that we could read a single line out of it.
+///     let mut conn = BufReader::new(conn);
+///     println!("Incoming connection!");
+///
+///     // Since our client example writes first, the server should read a line and only then send a
+///     // response. Otherwise, because reading and writing on a connection cannot be simultaneous
+///     // without threads or async, we can deadlock the two processes by having both sides wait for
+///     // the write buffer to be emptied by the other.
+///     conn.read_line(&mut buffer)?;
+///
+///     // Now that the read has come through and the client is waiting on the server's write, do
+///     // it. (`.get_mut()` is to get the writer, `BufReader` doesn't implement a pass-through
+///     // `Write`.)
+///     conn.get_mut().write_all(b"Hello from server!\n")?;
+///
+///     // Print out the result, getting the newline for free!
+///     print!("Client answered: {buffer}");
+///
+///     // Let's add an exit condition to shut the server down gracefully.
+///     if buffer == "stop\n" {
+///         break;
+///     }
+///
+///     // Clear the buffer so that the next iteration will display new data instead of messages
+///     // stacking on top of one another.
+///     buffer.clear();
+/// }
+/// # io::Result::<()>::Ok(())
+/// ```
 pub struct LocalSocketListener {
     inner: LocalSocketListenerImpl,
 }
--- src/local_socket/stream.rs
+++ src/local_socket/stream.rs
@@ -13,7 +13,49 @@ impmod! {local_socket,
 /// A local socket byte stream, obtained eiter from [`LocalSocketListener`] or by connecting to an existing local socket.
 ///
 /// # Examples
-/// - [Basic client](https://github.com/kotauskas/interprocess/blob/main/examples/local_socket/client.rs)
+/// Basic client:
+/// ```no_run
+/// use interprocess::local_socket::{LocalSocketStream, NameTypeSupport};
+/// use std::io::{prelude::*, BufReader};
+///
+/// // Pick a name. There isn't a helper function for this, mostly because it's largely unnecessary:
+/// // in Rust, `match` is your concise, readable and expressive decision making construct.
+/// let name = {
+///     // This scoping trick allows us to nicely contain the import inside the `match`, so that if
+///     // any imports of variants named `Both` happen down the line, they won't collide with the
+///     // enum we're working with here. Maybe someone should make a macro for this.
+///     use NameTypeSupport::*;
+///     match NameTypeSupport::query() {
+///         OnlyPaths => "/tmp/example.sock",
+///         OnlyNamespaced | Both => "@example.sock",
+///     }
+/// };
+///
+/// // Preemptively allocate a sizeable buffer for reading.
+/// // This size should be enough and should be easy to find for the allocator.
+/// let mut buffer = String::with_capacity(128);
+///
+/// // Create our connection. This will block until the server accepts our connection, but will fail
+/// // immediately if the server hasn't even started yet; somewhat similar to how happens with TCP,
+/// // where connecting to a port that's not bound to any server will send a "connection refused"
+/// // response, but that will take twice the ping, the roundtrip time, to reach the client.
+/// let conn = LocalSocketStream::connect(name)?;
+/// // Wrap it into a buffered reader right away so that we could read a single line out of it.
+/// let mut conn = BufReader::new(conn);
+///
+/// // Write our message into the stream. This will finish either when the whole message has been
+/// // writen or if a write operation returns an error. (`.get_mut()` is to get the writer,
+/// // `BufReader` doesn't implement a pass-through `Write`.)
+/// conn.get_mut().write_all(b"Hello from client!\n")?;
+///
+/// // We now employ the buffer we allocated prior and read until EOF, which the server will
+/// // similarly invoke with `.shutdown()`, verifying validity of UTF-8 on the fly.
+/// conn.read_line(&mut buffer)?;
+///
+/// // Print out the result, getting the newline for free!
+/// print!("Server answered: {buffer}");
+/// # std::io::Result::<()>::Ok(())
+/// ```
 ///
 /// [`LocalSocketListener`]: struct.LocalSocketListener.html " "
 pub struct LocalSocketStream {
