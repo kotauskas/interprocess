8ef2f813c15de5f6b048a9d20c219a3a9513987f
Kotauskas <v.toncharov@gmail.com>
2023-03-05 16:37:07 +0300

    Windows local socket reunite stub (no EH)

--- src/os/windows/local_socket/tokio/stream/mod.rs
+++ src/os/windows/local_socket/tokio/stream/mod.rs
@@ -10,7 +10,6 @@ use {
         local_socket::ToLocalSocketName,
         os::windows::named_pipe::{pipe_mode, tokio::DuplexPipeStream},
     },
-    futures_core::ready,
     futures_io::{AsyncRead, AsyncWrite},
     std::{
         ffi::c_void,
@@ -33,16 +32,25 @@ impl LocalSocketStream {
         let inner = DuplexPipeStream::connect(name.inner()).await?;
         Ok(Self { inner })
     }
+    #[inline]
     pub fn peer_pid(&self) -> io::Result<u32> {
         match self.inner.is_server() {
             true => self.inner.client_process_id(),
             false => self.inner.server_process_id(),
         }
     }
+    #[inline]
     pub fn into_split(self) -> (OwnedReadHalf, OwnedWriteHalf) {
         let (r, w) = self.inner.split();
         (OwnedReadHalf { inner: r }, OwnedWriteHalf { inner: w })
     }
+    pub fn reunite(rh: OwnedReadHalf, wh: OwnedWriteHalf) -> io::Result<Self> {
+        match rh.inner.reunite(wh.inner) {
+            Ok(inner) => Ok(Self { inner }),
+            Err(_) => todo!(),
+        }
+    }
+    #[inline]
     fn pinproj(&mut self) -> Pin<&mut StreamImpl> {
         Pin::new(&mut self.inner)
     }
@@ -51,20 +59,21 @@ impl LocalSocketStream {
 /// Thunks broken pipe errors into EOFs because broken pipe to the writer is what EOF is to the
 /// reader, but Windows shoehorns both into the former.
 impl AsyncRead for LocalSocketStream {
+    #[inline]
     fn poll_read(mut self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<io::Result<usize>> {
-        let rslt = self.pinproj().poll_read(cx, buf);
-        let thunked = ready!(rslt);
-        Poll::Ready(thunked)
+        self.pinproj().poll_read(cx, buf)
     }
 }
 impl AsyncWrite for LocalSocketStream {
+    #[inline]
     fn poll_write(mut self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<io::Result<usize>> {
         self.pinproj().poll_write(cx, buf)
     }
-    // Those two do nothing
+    #[inline]
     fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         self.pinproj().poll_flush(cx)
     }
+    #[inline]
     fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         self.pinproj().poll_close(cx)
     }
@@ -77,6 +86,7 @@ impl Debug for LocalSocketStream {
     }
 }
 impl AsRawHandle for LocalSocketStream {
+    #[inline]
     fn as_raw_handle(&self) -> *mut c_void {
         self.inner.as_raw_handle()
     }
