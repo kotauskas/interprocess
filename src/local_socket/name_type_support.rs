90e1bb477fac20048cf6ab9d7968d096cdeb9364
Kotauskas <v.toncharov@gmail.com>
2023-03-05 16:29:12 +0300

    Why would that ever implement `Hash`?

--- src/local_socket/name_type_support.rs
+++ src/local_socket/name_type_support.rs
@@ -4,7 +4,7 @@ impmod! {local_socket,
 }
 
 /// Represents which kinds of identifiers can be used for a local socket's name on the current platform.
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum NameTypeSupport {
     /// Only filesystem paths can be used.
     ///
@@ -22,17 +22,13 @@ pub enum NameTypeSupport {
 impl NameTypeSupport {
     /// The types of local socket names supported on the current platform regardless of the environment and OS version.
     ///
-    /// On most platforms, the value is known at compile time, i.e. the support for paths wasn't introduced in a specific version of the OS or isn't known to be supported at all. **Currently, this includes all supported OSes.** If support is added for an OS which added this functionality in a specific version, this constant will be the most restrictive value for that platform, with [`query`] possibly returning the actual value according to the current version of the OS.
+    /// On most platforms, the value is known at compile time, i.e. the support for paths wasn't introduced in a specific version of the OS or isn't known to be supported at all. **Currently, this includes all supported OSes.** If support is added for an OS which added this functionality in a specific version, this constant will be the most restrictive value for that platform, with [`query`](Self::query) possibly returning the actual value according to the current version of the OS.
     ///
     /// Simply put, you should probably just use this value for consistency across platforms, unless you really need a specific name type to be supported.
-    ///
-    /// [`query`]: #method.query " "
     pub const ALWAYS_AVAILABLE: Self = NAME_TYPE_ALWAYS_SUPPORTED_REAL;
     /// Returns the types of local socket names supported on the current platform with the current environment.
     ///
-    /// On most platforms, the value is known at compile time, i.e. the support for one of the types wasn't introduced in an update to the OS or isn't known to be supported at all. **Currently, this includes all supported OSes.** For compatibility with OSes which might add the functionality in the future starting with a specific version, this function isn't a `const fn` – see [`ALWAYS_AVAILABLE`] if you need a constant expression.
-    ///
-    /// [`ALWAYS_AVAILABLE`]: #associatedconstant.ALWAYS_AVAILABLE " "
+    /// On most platforms, the value is known at compile time, i.e. the support for one of the types wasn't introduced in an update to the OS or isn't known to be supported at all. **Currently, this includes all supported OSes.** For compatibility with OSes which might add the functionality in the future starting with a specific version, this function isn't a `const fn` – see [`ALWAYS_AVAILABLE`](Self::ALWAYS_AVAILABLE) if you need a constant expression.
     pub fn query() -> Self {
         name_type_support_query_impl()
     }
