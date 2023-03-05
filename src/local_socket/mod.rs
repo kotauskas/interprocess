361bcb94639fb59907dd9bd7f0d91edf1f864594
Kotauskas <v.toncharov@gmail.com>
2023-03-05 16:28:02 +0300

    Adjust local socket docs a little

--- src/local_socket/mod.rs
+++ src/local_socket/mod.rs
@@ -1,15 +1,22 @@
 //! Local sockets, an IPC primitive featuring a server and multiple clients connecting to that server using a filesystem path inside a special namespace, each having a private connection to that server.
 //!
-//! Local sockets are not a real IPC method implemented by the OS – they were introduced because of the difference between named pipes on Windows and Unix: named pipes on Windows are almost the same as Unix domain sockets on Linux while Unix named pipes (which are referred to as FIFO files in this crate to avoid confusion) are like unnamed pipes but identifiable with a filesystem path: there's no distinction between writers and the first reader takes all. **Simply put, local sockets use named pipes on Windows and Unix domain sockets on Unix.**
+//! ## Implementation types
+//! Local sockets are not a real IPC method implemented by the OS – they exist to smooth out the difference between two types of underlying implementation: **Unix domain sockets** and **Windows named pipes**. The [`ImplType`] enumeration documents them and provides methods to query whether they are available and their implementation specifics.
+//!
+//! ### Implementation properties
+//! Implementations of the exact same IPC primitive can have subtly different feature sets on different platforms and even on different versions of the same OS. For example, only on Linux and Windows do Ud-sockets support the "anonymous namespace" (and thus feature [`NameTypeSupport::Both`]); on FreeBSD, macOS and the likes, only file paths are available.
+//!
+//! The [`ImplProperties`] struct, as obtained through [`ImplType`]'s methods, is a source of information on all possible differences between different implementations of local sockets. This is to say that equal [`ImplProperties`] correspond to the same observable behavior of the IPC primitive – if there are any other differences that affect the public API but are not documented by [`ImplProperties`] (besides the mere fact that different IPC primitives use different system APIs), that's a bug in Interprocess!
+//!
+//! ### Platform-specific namespaces
+//! Since only Linux supports putting Ud-sockets in a separate namespace which is isolated from the filesystem, the `LocalSocketName`/`LocalSocketNameBuf` types are used to identify local sockets rather than `OsStr`/`OsString`: on Unix platforms other than Linux, which includes macOS, all flavors of BSD and possibly other Unix-like systems, the only way to name a Ud-socket is to use a filesystem path. As such, those platforms don't have the namespaced socket creation method available. Complicatng matters further, Windows does not support named pipes in the normal filesystem, meaning that namespaced local sockets are the only functional method on Windows.
+//!
+//! As a way to solve this issue, [`LocalSocketName`]/`LocalSocketNameBuf` only provide creation in a platform-specific way, meaning that crate users are required to query [`NameTypeSupport`] to decide on the socket names.
 //!
 //! ## Differences from regular sockets
 //! A few missing features, primarily on Windows, require local sockets to omit some important functionality, because code relying on it wouldn't be portable. Some notable differences are:
-//! - No `.shutdown()` – your communication protocol must manually negotiate end of transmission. Notably, `.read_to_string()` and `.read_all()` will always block indefinitely at some point. They will be changed to panic instead in a future version.
-//! - No vectored I/O – Windows named pipes use the Win32 file API, rather than the Winsock API, for read-write operations, so scatter-gather I/O works the same way it does with regular files, which is incompatible with the Rust standard library API for it.
-//! - No datagram sockets – the difference in semantics between connectionless datagram Ud-sockets and connection-based named message pipes on Windows does not allow bridging those two into a common API. Streams are fast enough anyway, so no big deal, right?
-//!
-//! ## Platform-specific namespaces
-//! There's one more problem regarding platform differences: since only Linux supports putting Ud-sockets in a separate namespace which is isolated from the filesystem, the `LocalSocketName`/`LocalSocketNameBuf` types are used to identify local sockets rather than `OsStr`/`OsString`: on Unix platforms other than Linux, which includes macOS, all flavors of BSD and possibly other Unix-like systems, the only way to name a Ud-socket is to use a filesystem path. As such, those platforms don't have the namespaced socket creation method available. Complicatng matters further, Windows does not support named pipes in the normal filesystem, meaning that namespaced local sockets are the only functional method on Windows. As a way to solve this issue, `LocalSocketName`/`LocalSocketNameBuf` only provide creation in a platform-specific way, meaning that crate users are required to use conditional compilation to decide on the socket names.
+//! - No `.shutdown()` – your communication protocol must manually negotiate end of transmission. Notably, `.read_to_string()` and `.read_all()` will always block indefinitely at some point.
+//! - No datagram sockets – the difference in semantics between connectionless datagram Ud-sockets and connection-based named message pipes on Windows does not allow bridging those two into a common API. You can emulate datagrams on top of streams anyway, so no big deal, right?
 
 #[cfg(feature = "tokio")]
 #[cfg_attr(feature = "doc_cfg", doc(cfg(feature = "tokio")))]
