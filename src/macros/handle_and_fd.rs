b46e84866339f96c4549e84fbfa6c35cef16b92e
Kotauskas <v.toncharov@gmail.com>
2023-06-14 19:55:27 +0300

    Improve the raw-derive macro a bit

--- src/macros/handle_and_fd.rs
+++ src/macros/handle_and_fd.rs
@@ -3,9 +3,13 @@
 // Derive macros that implement raw handle manipulation in terms of safe handle manipulation from Rust 1.63+.
 
 macro_rules! derive_asraw {
-    (@impl $ty:ident $(<$lt:lifetime>)?, $hty:ident, $trt:ident, $mtd:ident, $strt:ident, $smtd:ident, $cfg:ident) => {
+    (@impl
+        $({$($forcl:tt)*})?
+        $ty:ty,
+        $hty:ident, $trt:ident, $mtd:ident,
+        $strt:ident, $smtd:ident, $cfg:ident) => {
         #[cfg($cfg)]
-        impl ::std::os::$cfg::io::$trt for $ty $(<$lt>)? {
+        impl $(<$($forcl)*>)? ::std::os::$cfg::io::$trt for $ty {
             #[inline]
             fn $mtd(&self) -> ::std::os::$cfg::io::$hty {
                 let h = ::std::os::$cfg::io::$strt::$smtd(self);
@@ -13,22 +17,34 @@ macro_rules! derive_asraw {
             }
         }
     };
-    (windows: $ty:ident $(<$lt:lifetime>)?) => {
-        derive_asraw!(@impl $ty $(<$lt>)?, RawHandle, AsRawHandle, as_raw_handle, AsHandle, as_handle, windows);
+    (windows: $({$($forcl:tt)*})? $ty:ty) => {
+        derive_asraw!(
+            @impl
+            $({$($forcl)*})? $ty,
+            RawHandle, AsRawHandle, as_raw_handle,
+            AsHandle, as_handle, windows);
     };
-    (unix: $ty:ident $(<$lt:lifetime>)?) => {
-        derive_asraw!(@impl $ty $(<$lt>)?, RawFd, AsRawFd, as_raw_fd, AsFd, as_fd, unix);
+    (unix: $({$($forcl:tt)*})? $ty:ty) => {
+        derive_asraw!(
+            @impl
+            $({$($forcl)*})? $ty,
+            RawFd, AsRawFd, as_raw_fd,
+            AsFd, as_fd, unix);
     };
-    ($ty:ident) => {
-        derive_asraw!(windows: $ty);
-        derive_asraw!(unix: $ty);
+    ($({$($forcl:tt)*})? $ty:ty) => {
+        derive_asraw!(windows: $({$($forcl)*})? $ty);
+        derive_asraw!(unix: $({$($forcl)*})? $ty);
     };
 }
 
 macro_rules! derive_intoraw {
-    (@impl $ty:ident, $hty:ident, $ohty:ident, $trt:ident, $mtd:ident, $cfg:ident) => {
+    (@impl
+        $({$($forcl:tt)*})?
+        $ty:ty,
+        $hty:ident, $ohty:ident,
+        $trt:ident, $mtd:ident, $cfg:ident) => {
         #[cfg($cfg)]
-        impl ::std::os::$cfg::io::$trt for $ty {
+        impl $(<$($forcl)*>)? ::std::os::$cfg::io::$trt for $ty {
             #[inline]
             fn $mtd(self) -> ::std::os::$cfg::io::$hty {
                 let h = <std::os::$cfg::io::$ohty as ::std::convert::From<_>>::from(self);
@@ -36,37 +52,49 @@ macro_rules! derive_intoraw {
             }
         }
     };
-    (windows: $ty:ident) => {
-        derive_intoraw!(@impl $ty, RawHandle, OwnedHandle, IntoRawHandle, into_raw_handle, windows);
+    (windows: $({$($forcl:tt)*})? $ty:ty) => {
+        derive_intoraw!(
+            @impl
+            $({$($forcl)*})? $ty,
+            RawHandle, OwnedHandle,
+            IntoRawHandle, into_raw_handle, windows);
     };
-    (unix: $ty:ident) => {
-        derive_intoraw!(@impl $ty, RawFd, OwnedFd, IntoRawFd, into_raw_fd, unix);
+    (unix: $({$($forcl:tt)*})? $ty:ty) => {
+        derive_intoraw!(
+            @impl
+            $({$($forcl)*})? $ty,
+            RawFd, OwnedFd,
+            IntoRawFd, into_raw_fd, unix);
     };
-    ($ty:ident) => {
-        derive_intoraw!(windows: $ty);
-        derive_intoraw!(unix: $ty);
+    ($({$($forcl:tt)*})? $ty:ty) => {
+        derive_intoraw!(windows: $({$($forcl)*})? $ty);
+        derive_intoraw!(unix: $({$($forcl)*})? $ty);
     };
 }
 
 macro_rules! derive_asintoraw {
-    (windows: $ty:ident) => {
-        derive_asraw!(windows: $ty);
-        derive_intoraw!(windows: $ty);
+    (windows: $({$($forcl:tt)*})? $ty:ty) => {
+        derive_asraw!(windows: $({$($forcl)*})? $ty);
+        derive_intoraw!(windows: $({$($forcl)*})? $ty);
     };
-    (unix: $ty:ident) => {
-        derive_asraw!(unix: $ty);
-        derive_intoraw!(unix: $ty);
+    (unix: $({$($forcl:tt)*})? $ty:ty) => {
+        derive_asraw!(unix: $({$($forcl)*})? $ty);
+        derive_intoraw!(unix: $({$($forcl)*})? $ty);
     };
-    ($ty:ident) => {
-        derive_asintoraw!(windows: $ty);
-        derive_asintoraw!(unix: $ty);
+    ($({$($forcl:tt)*})? $ty:ty) => {
+        derive_asintoraw!(windows: $({$($forcl)*})? $ty);
+        derive_asintoraw!(unix: $({$($forcl)*})? $ty);
     };
 }
 
 macro_rules! derive_fromraw {
-    (@impl $ty:ident, $hty:ident, $ohty:ident, $trt:ident, $mtd:ident, $cfg:ident) => {
+    (@impl
+        $({$($forcl:tt)*})?
+        $ty:ty,
+        $hty:ident, $ohty:ident,
+        $trt:ident, $mtd:ident, $cfg:ident) => {
         #[cfg($cfg)]
-        impl ::std::os::$cfg::io::$trt for $ty {
+        impl $(<$($forcl)*>)? ::std::os::$cfg::io::$trt for $ty {
             #[inline]
             unsafe fn $mtd(fd: ::std::os::$cfg::io::$hty) -> Self {
                 let h: ::std::os::$cfg::io::$ohty = unsafe { ::std::os::$cfg::io::$trt::$mtd(fd) };
@@ -74,30 +102,38 @@ macro_rules! derive_fromraw {
             }
         }
     };
-    (windows: $ty:ident) => {
-        derive_fromraw!(@impl $ty, RawHandle, OwnedHandle, FromRawHandle, from_raw_handle, windows);
+    (windows: $({$($forcl:tt)*})? $ty:ty) => {
+        derive_fromraw!(
+            @impl
+            $({$($forcl)*})? $ty,
+            RawHandle, OwnedHandle,
+            FromRawHandle, from_raw_handle, windows);
     };
-    (unix: $ty:ident) => {
-        derive_fromraw!(@impl $ty, RawFd, OwnedFd, FromRawFd, from_raw_fd, unix);
+    (unix: $({$($forcl:tt)*})? $ty:ty) => {
+        derive_fromraw!(
+            @impl
+            $({$($forcl)*})? $ty,
+            RawFd, OwnedFd,
+            FromRawFd, from_raw_fd, unix);
     };
-    ($ty:ident) => {
-        derive_fromraw!(windows: $ty);
-        derive_fromraw!(unix: $ty);
+    ($({$($forcl:tt)*})? $ty:ty) => {
+        derive_fromraw!(windows: $({$($forcl)*})? $ty);
+        derive_fromraw!(unix: $({$($forcl)*})? $ty);
     };
 }
 
 macro_rules! derive_raw {
-    (windows: $ty:ident) => {
-        derive_asintoraw!(windows: $ty);
-        derive_fromraw!(windows: $ty);
+    (windows: $({$($forcl:tt)*})? $ty:ty) => {
+        derive_asintoraw!(windows: $({$($forcl)*})? $ty);
+        derive_fromraw!(windows: $({$($forcl)*})? $ty);
     };
-    (unix: $ty:ident) => {
-        derive_asintoraw!(unix: $ty);
-        derive_fromraw!(unix: $ty);
+    (unix: $({$($forcl:tt)*})? $ty:ty) => {
+        derive_asintoraw!(unix: $({$($forcl)*})? $ty);
+        derive_fromraw!(unix: $({$($forcl)*})? $ty);
     };
-    ($ty:ident) => {
-        derive_asintoraw!($ty);
-        derive_fromraw!($ty);
+    ($({$($forcl:tt)*})? $ty:ty) => {
+        derive_asintoraw!($({$($forcl)*})? $ty);
+        derive_fromraw!($({$($forcl)*})? $ty);
     };
 }
 
@@ -105,92 +141,90 @@ macro_rules! derive_raw {
 // by one of the above derives.
 
 macro_rules! forward_as_handle {
-    (@impl $ty:ident, $fld:ident, $hty:ident, $trt:ident, $mtd:ident, $cfg:ident) => {
+    (@impl $ty:ident, $hty:ident, $trt:ident, $mtd:ident, $cfg:ident) => {
         #[cfg($cfg)]
         impl ::std::os::$cfg::io::$trt for $ty {
             #[inline]
             fn $mtd(&self) -> ::std::os::$cfg::io::$hty<'_> {
-                ::std::os::$cfg::io::$trt::$mtd(&self.$fld)
+                ::std::os::$cfg::io::$trt::$mtd(&self.0)
             }
         }
     };
-    (windows: $ty:ident, $fld:ident) => {
-        forward_as_handle!(@impl $ty, $fld, BorrowedHandle, AsHandle, as_handle, windows);
+    (windows: $ty:ident) => {
+        forward_as_handle!(@impl $ty, BorrowedHandle, AsHandle, as_handle, windows);
     };
-    (unix: $ty:ident, $fld:ident) => {
-        forward_as_handle!(@impl $ty, $fld, BorrowedFd, AsFd, as_fd, unix);
+    (unix: $ty:ident) => {
+        forward_as_handle!(@impl $ty, BorrowedFd, AsFd, as_fd, unix);
     };
-    ($ty:ident, $fld:ident) => {
-        forward_as_handle!(windows: $ty, $fld);
-        forward_as_handle!(unix: $ty, $fld);
+    ($ty:ident) => {
+        forward_as_handle!(windows: $ty);
+        forward_as_handle!(unix: $ty);
     };
 }
 
 macro_rules! forward_into_handle {
-    (@impl $ty:ident, $fld:ident, $hty:ident, $cfg:ident) => {
+    (@impl $ty:ident, $hty:ident, $cfg:ident) => {
         #[cfg($cfg)]
         impl ::std::convert::From<$ty> for ::std::os::$cfg::io::$hty {
             #[inline]
             fn from(x: $ty) -> Self {
-                ::std::convert::From::from(x.$fld)
+                ::std::convert::From::from(x.0)
             }
         }
     };
-    (windows: $ty:ident, $fld:ident) => {
-        forward_into_handle!(@impl $ty, $fld, OwnedHandle, windows);
+    (windows: $ty:ident) => {
+        forward_into_handle!(@impl $ty, OwnedHandle, windows);
     };
-    (unix: $ty:ident, $fld:ident) => {
-        forward_into_handle!(@impl $ty, $fld, OwnedFd, unix);
+    (unix: $ty:ident) => {
+        forward_into_handle!(@impl $ty, OwnedFd, unix);
     };
-    ($ty:ident, $fld:ident) => {
-        forward_into_handle!(windows: $ty, $fld);
-        forward_into_handle!(unix: $ty, $fld);
+    ($ty:ident) => {
+        forward_into_handle!(windows: $ty);
+        forward_into_handle!(unix: $ty);
     };
 }
 
 macro_rules! forward_from_handle {
-    (@impl $ty:ident, $fld:ident, $hty:ident, $cfg:ident) => {
+    (@impl $ty:ident, $hty:ident, $cfg:ident) => {
         #[cfg($cfg)]
         impl ::std::convert::From<::std::os::$cfg::io::$hty> for $ty {
             #[inline]
             fn from(x: ::std::os::$cfg::io::$hty) -> Self {
-                Self {
-                    $fld: ::std::convert::From::from(x),
-                }
+                Self(::std::convert::From::from(x))
             }
         }
     };
-    (windows: $ty:ident, $fld:ident) => {
-        forward_from_handle!(@impl $ty, $fld, OwnedHandle, windows);
+    (windows: $ty:ident) => {
+        forward_from_handle!(@impl $ty, OwnedHandle, windows);
     };
-    (unix: $ty:ident, $fld:ident) => {
-        forward_from_handle!(@impl $ty, $fld, OwnedFd, unix);
+    (unix: $ty:ident) => {
+        forward_from_handle!(@impl $ty, OwnedFd, unix);
     };
-    ($ty:ident, $fld:ident) => {
-        forward_from_handle!(windows: $ty, $fld);
-        forward_from_handle!(unix: $ty, $fld);
+    ($ty:ident) => {
+        forward_from_handle!(windows: $ty);
+        forward_from_handle!(unix: $ty);
     };
 }
 
 macro_rules! forward_handle {
-    (windows: $ty:ident, $fld:ident) => {
-        forward_as_handle!(windows: $ty, $fld);
-        forward_into_handle!(windows: $ty, $fld);
-        forward_from_handle!(windows: $ty, $fld);
+    (windows: $ty:ident) => {
+        forward_as_handle!(windows: $ty);
+        forward_into_handle!(windows: $ty);
+        forward_from_handle!(windows: $ty);
     };
-    (unix: $ty:ident, $fld:ident) => {
-        forward_as_handle!(unix: $ty, $fld);
-        forward_into_handle!(unix: $ty, $fld);
-        forward_from_handle!(unix: $ty, $fld);
+    (unix: $ty:ident) => {
+        forward_as_handle!(unix: $ty);
+        forward_into_handle!(unix: $ty);
+        forward_from_handle!(unix: $ty);
     };
-    ($ty:ident, $fld:ident) => {
-        forward_handle!(windows: $ty, $fld);
-        forward_handle!(unix: $ty, $fld);
+    ($ty:ident) => {
+        forward_handle!(windows: $ty);
+        forward_handle!(unix: $ty);
     };
 }
 
 macro_rules! forward_try_into_handle {
-    (@impl $ty:ident, $fld:ident, $fldt:path, $hty:ident, $cfg:ident) => {
+    (@impl $ty:ident, $fldt:path, $hty:ident, $cfg:ident) => {
         /// Releases ownership of the handle/file descriptor, detaches the object from the async runtime and returns the handle/file descriptor as an owned object.
         ///
         /// # Errors
@@ -200,24 +234,24 @@ macro_rules! forward_try_into_handle {
             type Error = <$fldt as ::std::convert::TryFrom<::std::os::$cfg::io::$hty>>::Error;
             #[inline]
             fn try_from(x: $ty) -> Result<Self, Self::Error> {
-                ::std::convert::TryFrom::try_from(x.$fld)
+                ::std::convert::TryFrom::try_from(x.0)
             }
         }
     };
-    (windows: $ty:ident, $fld:ident, $fldt:path) => {
-        forward_try_into_handle!(@impl $ty, $fld, $fldt, OwnedHandle, windows);
+    (windows: $ty:ident, $fldt:path) => {
+        forward_try_into_handle!(@impl $ty, $fldt, OwnedHandle, windows);
     };
-    (unix: $ty:ident, $fld:ident, $fldt:path) => {
-        forward_try_into_handle!(@impl $ty, $fld, $fldt, OwnedFd, unix);
+    (unix: $ty:ident, $fldt:path) => {
+        forward_try_into_handle!(@impl $ty, $fldt, OwnedFd, unix);
     };
-    ($ty:ident, $fld:ident, $fldt:path) => {
-        forward_try_into_handle!(windows: $ty, $fld, $fldt);
-        forward_try_into_handle!(unix: $ty, $fld, $fldt);
+    ($ty:ident, $fldt:path) => {
+        forward_try_into_handle!(windows: $ty);
+        forward_try_into_handle!(unix: $ty);
     };
 }
 
 macro_rules! forward_try_from_handle {
-    (@impl $ty:ident, $fld:ident, $fldt:path, $hty:ident, $cfg:ident) => {
+    (@impl $ty:ident, $fldt:path, $hty:ident, $cfg:ident) => {
         /// Creates an async object from a given owned handle/file descriptor. This will also attach the object to the async runtime this function is called in.
         ///
         /// # Errors
@@ -227,144 +261,33 @@ macro_rules! forward_try_from_handle {
             type Error = <$fldt as ::std::convert::TryFrom<::std::os::$cfg::io::$hty>>::Error;
             #[inline]
             fn try_from(x: ::std::os::$cfg::io::$hty) -> Result<Self, Self::Error> {
-                Ok(Self {
-                    $fld: ::std::convert::TryFrom::try_from(x)?,
-                })
+                Ok(Self(::std::convert::TryFrom::try_from(x)?))
             }
         }
     };
-    (windows: $ty:ident, $fld:ident, $fldt:path) => {
-        forward_try_from_handle!(@impl $ty, $fld, $fldt, OwnedHandle, windows);
+    (windows: $ty:ident, $fldt:path) => {
+        forward_try_from_handle!(@impl $ty, $fldt, OwnedHandle, windows);
     };
-    (unix: $ty:ident, $fld:ident, $fldt:path) => {
-        forward_try_from_handle!(@impl $ty, $fld, $fldt, OwnedFd, unix);
+    (unix: $ty:ident, $fldt:path) => {
+        forward_try_from_handle!(@impl $ty, $fldt, OwnedFd, unix);
     };
-    ($ty:ident, $fld:ident, $fldt:path) => {
-        forward_try_from_handle!(windows: $ty, $fld, $fldt);
-        forward_try_from_handle!(unix: $ty, $fld, $fldt);
+    ($ty:ident, $fldt:path) => {
+        forward_try_from_handle!(windows: $ty, $fldt);
+        forward_try_from_handle!(unix: $ty, $fldt);
     };
 }
 
 macro_rules! forward_try_handle {
-    (windows: $ty:ident, $fld:ident, $fldt:path) => {
-        forward_try_into_handle!(windows: $ty, $fld, $fldt);
-        forward_try_from_handle!(windows: $ty, $fld, $fldt);
+    (windows: $ty:ident, $fldt:path) => {
+        forward_try_into_handle!(windows: $ty, $fldt);
+        forward_try_from_handle!(windows: $ty, $fldt);
     };
-    (unix: $ty:ident, $fld:ident, $fldt:path) => {
-        forward_try_into_handle!(unix: $ty, $fld, $fldt);
-        forward_try_from_handle!(unix: $ty, $fld, $fldt);
+    (unix: $ty:ident, $fldt:path) => {
+        forward_try_into_handle!(unix: $ty, $fldt);
+        forward_try_from_handle!(unix: $ty, $fldt);
     };
-    ($ty:ident, $fld:ident, $fldt:path) => {
-        forward_try_handle!(windows: $ty, $fld, $fldt);
-        forward_try_handle!(unix: $ty, $fld, $fldt);
-    };
-}
-
-// Legacy macros.
-/*
-macro_rules! impl_as_raw_handle_windows {
-    ($ty:ident) => {
-        #[cfg(windows)]
-        impl ::std::os::windows::io::AsRawHandle for $ty {
-            fn as_raw_handle(&self) -> *mut ::std::ffi::c_void {
-                ::std::os::windows::io::AsRawHandle::as_raw_handle(&self.inner)
-            }
-        }
+    ($ty:ident, $fldt:path) => {
+        forward_try_handle!(windows: $ty, $fldt);
+        forward_try_handle!(unix: $ty, $fldt);
     };
 }
-macro_rules! impl_as_raw_handle_unix {
-    ($ty:ident) => {
-        #[cfg(unix)]
-        impl ::std::os::unix::io::AsRawFd for $ty {
-            fn as_raw_fd(&self) -> ::libc::c_int {
-                ::std::os::unix::io::AsRawFd::as_raw_fd(&self.inner)
-            }
-        }
-    };
-}
-macro_rules! impl_as_raw_handle {
-    ($ty:ident) => {
-        impl_as_raw_handle_windows!($ty);
-        impl_as_raw_handle_unix!($ty);
-    };
-}
-
-macro_rules! impl_into_raw_handle_windows {
-    ($ty:ident) => {
-        #[cfg(windows)]
-        impl ::std::os::windows::io::IntoRawHandle for $ty {
-            fn into_raw_handle(self) -> *mut ::std::ffi::c_void {
-                ::std::os::windows::io::IntoRawHandle::into_raw_handle(self.inner)
-            }
-        }
-    };
-}
-macro_rules! impl_into_raw_handle_unix {
-    ($ty:ident) => {
-        #[cfg(unix)]
-        impl ::std::os::unix::io::IntoRawFd for $ty {
-            fn into_raw_fd(self) -> ::libc::c_int {
-                ::std::os::unix::io::IntoRawFd::into_raw_fd(self.inner)
-            }
-        }
-    };
-}
-macro_rules! impl_into_raw_handle {
-    ($ty:ident) => {
-        impl_into_raw_handle_windows!($ty);
-        impl_into_raw_handle_unix!($ty);
-    };
-}
-
-macro_rules! impl_from_raw_handle_windows {
-    ($ty:ident) => {
-        #[cfg(windows)]
-        impl ::std::os::windows::io::FromRawHandle for $ty {
-            unsafe fn from_raw_handle(handle: *mut ::std::ffi::c_void) -> Self {
-                Self {
-                    inner: unsafe { ::std::os::windows::io::FromRawHandle::from_raw_handle(handle) },
-                }
-            }
-        }
-    };
-}
-macro_rules! impl_from_raw_handle_unix {
-    ($ty:ident) => {
-        #[cfg(unix)]
-        impl ::std::os::unix::io::FromRawFd for $ty {
-            unsafe fn from_raw_fd(fd: ::libc::c_int) -> Self {
-                Self {
-                    inner: unsafe { ::std::os::unix::io::FromRawFd::from_raw_fd(fd) },
-                }
-            }
-        }
-    };
-}
-macro_rules! impl_from_raw_handle {
-    ($ty:ident) => {
-        impl_from_raw_handle_windows!($ty);
-        impl_from_raw_handle_unix!($ty);
-    };
-}
-
-macro_rules! impl_handle_manip_unix {
-    ($ty:ident) => {
-        impl_as_raw_handle_unix!($ty);
-        impl_into_raw_handle_unix!($ty);
-        impl_from_raw_handle_unix!($ty);
-    };
-}
-macro_rules! impl_handle_manip_windows {
-    ($ty:ident) => {
-        impl_as_raw_handle_windows!($ty);
-        impl_into_raw_handle_windows!($ty);
-        impl_from_raw_handle_windows!($ty);
-    };
-}
-macro_rules! impl_handle_manip {
-    ($ty:ident) => {
-        impl_handle_manip_unix!($ty);
-        impl_handle_manip_windows!($ty);
-    };
-}
-*/
