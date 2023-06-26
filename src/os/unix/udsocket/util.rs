191bb097c74824a61840eb35b080188c8cad3f11
Kotauskas <v.toncharov@gmail.com>
2023-06-26 17:04:43 +0300

    Cloexec on received FDs on Linux
    Would need to set this manually on other Unix systems

--- src/os/unix/udsocket/util.rs
+++ src/os/unix/udsocket/util.rs
@@ -109,6 +109,10 @@ pub fn make_msghdr_r(bufs: &mut [IoSliceMut<'_>], abuf: &mut CmsgMut<'_>) -> io:
         to_msghdr_iovlen(bufs.len())?,
     );
     abuf.fill_msghdr(&mut hdr, true)?;
+    #[cfg(target_os = "linux")]
+    {
+        hdr.msg_flags |= libc::MSG_CMSG_CLOEXEC;
+    }
     Ok(hdr)
 }
 pub fn make_msghdr_w(bufs: &[IoSlice<'_>], abuf: CmsgRef<'_>) -> io::Result<msghdr> {
