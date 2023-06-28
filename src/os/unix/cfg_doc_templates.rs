6a73161cb8abb02a1f13b6c458bc0e8b2ea96f2a
Kotauskas <v.toncharov@gmail.com>
2023-06-28 19:06:27 +0300

    Will be useful later

--- src/os/unix/cfg_doc_templates.rs
+++ src/os/unix/cfg_doc_templates.rs
@@ -10,6 +10,19 @@
     )))
 )]
 
+#[cfg_attr( // uds_cmsgcred template
+    feature = "doc_cfg",
+    doc(cfg(any(
+        target_os = "freebsd",
+        target_os = "dragonfly"
+    )))
+)]
+
+#[cfg_attr( // uds_sockcred template
+    feature = "doc_cfg",
+    doc(cfg(target_os = "freebsd"))
+)]
+
 #[cfg_attr( // uds_linux_namespace template
     feature = "doc_cfg",
     doc(cfg(any(target_os = "linux", target_os = "android")))
