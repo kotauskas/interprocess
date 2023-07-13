672f6dc168ba38fe1722f0ae9802f24d79004fc4
Kotauskas <v.toncharov@gmail.com>
2023-07-13 16:01:09 +0300

    Port `UdStream` to the new traits

--- src/os/unix/udsocket/stream.rs
+++ src/os/unix/udsocket/stream.rs
@@ -1,8 +1,9 @@
 use super::{
     c_wrappers,
-    cmsg::{context::Collector, CmsgMutBuf, CmsgRef},
+    cmsg::{context::Collector, CmsgMut, CmsgMutExt, CmsgRef},
+    read_in_terms_of_vectored,
     util::{make_msghdr_r, make_msghdr_w},
-    ToUdSocketPath, UdSocketPath,
+    write_in_terms_of_vectored, ReadAncillary, ReadAncillarySuccess, ToUdSocketPath, UdSocketPath, WriteAncillary,
 };
 use crate::{
     os::unix::{unixprelude::*, FdOps},
@@ -58,68 +59,6 @@ impl UdStream {
 
         Ok(Self(fd))
     }
-
-    /// Receives both bytes and ancillary data from the socket stream. The first element of the return value
-    /// represents the read amount of the former, while the second element represents that of the latter.
-    ///
-    /// # System calls
-    /// - `recvmsg`
-    #[inline]
-    pub fn recv_ancillary<E: Collector>(
-        &self,
-        buf: &mut [u8],
-        abuf: &mut CmsgMutBuf<'_, E>,
-    ) -> io::Result<(usize, usize)> {
-        self.recv_ancillary_vectored(&mut [IoSliceMut::new(buf)], abuf)
-    }
-    /// Receives bytes and ancillary data from the socket stream, making use of [scatter input] for the main data.
-    /// The first element of the return value represents the read amount of the former, while the second element
-    /// represents that of the latter.
-    ///
-    /// # System calls
-    /// - `recvmsg`
-    ///
-    /// [scatter input]: https://en.wikipedia.org/wiki/Vectored_I/O " "
-    #[inline]
-    pub fn recv_ancillary_vectored<E: Collector>(
-        &self,
-        bufs: &mut [IoSliceMut<'_>],
-        abuf: &mut CmsgMutBuf<'_, E>,
-    ) -> io::Result<(usize, usize)> {
-        let mut hdr = make_msghdr_r(bufs, abuf)?;
-        let fd = self.as_fd();
-
-        abuf.context_collector.pre_op_collect(fd);
-        let bytes_read = unsafe {
-            // SAFETY: make_msghdr_r is good at its job
-            c_wrappers::recvmsg(fd, &mut hdr, 0)?
-        };
-        abuf.context_collector.post_op_collect(fd, hdr.msg_flags);
-
-        Ok((bytes_read, hdr.msg_controllen as _))
-    }
-
-    /// Sends bytes and ancillary data into the socket stream.
-    ///
-    /// # System calls
-    /// - `sendmsg`
-    #[inline]
-    pub fn send_ancillary(&self, buf: &[u8], abuf: CmsgRef<'_, '_>) -> io::Result<usize> {
-        self.send_ancillary_vectored(&[IoSlice::new(buf)], abuf)
-    }
-    /// Sends bytes and ancillary data into the socket stream, making use of [gather output] for the main data.
-    ///
-    /// # System calls
-    /// - `sendmsg`
-    ///
-    /// [gather output]: https://en.wikipedia.org/wiki/Vectored_I/O " "
-    pub fn send_ancillary_vectored(&self, bufs: &[IoSlice<'_>], abuf: CmsgRef<'_, '_>) -> io::Result<usize> {
-        let hdr = make_msghdr_w(bufs, abuf)?;
-        unsafe {
-            // SAFETY: make_msghdr_w is good at its job
-            c_wrappers::sendmsg(self.as_fd(), &hdr, 0)
-        }
-    }
 }
 
 /// A list of used system calls is available.
@@ -150,6 +89,68 @@ impl Read for UdStream {
         (&*self).read_vectored(bufs)
     }
 }
+
+/// A list of used system calls is available.
+impl<AB: CmsgMut + ?Sized> ReadAncillary<AB> for &UdStream {
+    /// Implemented in terms of `read_ancillary_vectored`.
+    ///
+    /// # System calls
+    /// - `recvmsg`
+    #[inline]
+    fn read_ancillary(&mut self, buf: &mut [u8], abuf: &mut AB) -> io::Result<ReadAncillarySuccess> {
+        read_in_terms_of_vectored(self, buf, abuf)
+    }
+    /// # System calls
+    /// - `recvmsg`
+    fn read_ancillary_vectored(
+        &mut self,
+        bufs: &mut [IoSliceMut<'_>],
+        abuf: &mut AB,
+    ) -> io::Result<ReadAncillarySuccess> {
+        let mut hdr = make_msghdr_r(bufs, abuf)?;
+        let fd = self.as_fd();
+
+        abuf.context_mut().pre_op_collect(fd);
+        let bytes_read = unsafe {
+            // SAFETY: make_msghdr_r is good at its job
+            c_wrappers::recvmsg(fd, &mut hdr, 0)?
+        };
+        abuf.context_mut().post_op_collect(fd, hdr.msg_flags);
+
+        let advanc = hdr.msg_controllen as _; // FIXME as casts are bad!!
+        unsafe {
+            // SAFETY: let's hope that recvmsg doesn't just straight up lie to us on the success path
+            abuf.add_len(advanc);
+        }
+
+        Ok(ReadAncillarySuccess {
+            main: bytes_read,
+            ancillary: advanc,
+        })
+    }
+}
+/// A list of used system calls is available.
+impl<AB: CmsgMut + ?Sized> ReadAncillary<AB> for UdStream {
+    /// Implemented in terms of `read_ancillary_vectored()`.
+    ///
+    /// # System calls
+    /// - `recvmsg`
+    #[inline(always)]
+    fn read_ancillary(&mut self, buf: &mut [u8], abuf: &mut AB) -> io::Result<ReadAncillarySuccess> {
+        (&*self).read_ancillary(buf, abuf)
+    }
+    /// # System calls
+    /// - `recvmsg`
+    #[inline(always)]
+    fn read_ancillary_vectored(
+        &mut self,
+        bufs: &mut [IoSliceMut<'_>],
+        abuf: &mut AB,
+    ) -> io::Result<ReadAncillarySuccess> {
+        (&*self).read_ancillary_vectored(bufs, abuf)
+    }
+}
+
 /// A list of used system calls is available.
 impl Write for &UdStream {
     /// # System calls
@@ -188,6 +189,42 @@ impl Write for UdStream {
     fn flush(&mut self) -> io::Result<()> {
         // You cannot flush a socket
         Ok(())
+    }
+}
+
+/// A list of used system calls is available.
+impl WriteAncillary for &UdStream {
+    /// Implemented in terms of `write_ancillary_vectored()`.
+    ///
+    /// # System calls
+    /// - `sendmsg`
+    #[inline]
+    fn write_ancillary(&mut self, buf: &[u8], abuf: CmsgRef<'_, '_>) -> io::Result<usize> {
+        write_in_terms_of_vectored(self, buf, abuf)
+    }
+    /// # System calls
+    /// - `sendmsg`
+    fn write_ancillary_vectored(&mut self, bufs: &[IoSlice<'_>], abuf: CmsgRef<'_, '_>) -> io::Result<usize> {
+        let hdr = make_msghdr_w(bufs, abuf)?;
+        unsafe {
+            // SAFETY: make_msghdr_w is good at its job
+            c_wrappers::sendmsg(self.as_fd(), &hdr, 0)
+        }
+    }
+}
+/// A list of used system calls is available.
+impl WriteAncillary for UdStream {
+    /// Implemented in terms of `write_ancillary_vectored()`.
+    ///
+    /// # System calls
+    /// - `sendmsg`
+    fn write_ancillary(&mut self, buf: &[u8], abuf: CmsgRef<'_, '_>) -> io::Result<usize> {
+        (&*self).write_ancillary(buf, abuf)
+    }
+    /// # System calls
+    /// - `sendmsg`
+    fn write_ancillary_vectored(&mut self, bufs: &[IoSlice<'_>], abuf: CmsgRef<'_, '_>) -> io::Result<usize> {
+        (&*self).write_ancillary_vectored(bufs, abuf)
     }
 }
 
