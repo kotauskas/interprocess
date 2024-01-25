5b65a24da1e1a80af0af81f73de2a8379bfe1235
Kotauskas <v.toncharov@gmail.com>
2024-01-25 17:06:52 +0300

    Should I?

--- src/os/windows/c_wrappers.rs
+++ src/os/windows/c_wrappers.rs
@@ -16,11 +16,14 @@ pub fn duplicate_handle(handle: BorrowedHandle<'_>) -> io::Result<OwnedHandle> {
 pub fn duplicate_handle_to_foreign(
     handle: BorrowedHandle<'_>,
     other_process: BorrowedHandle<'_>,
-) -> io::Result<HANDLE> {
+) -> io::Result<RawHandle> {
     duplicate_handle_inner(handle, Some(other_process))
 }
 
-fn duplicate_handle_inner(handle: BorrowedHandle<'_>, other_process: Option<BorrowedHandle<'_>>) -> io::Result<HANDLE> {
+fn duplicate_handle_inner(
+    handle: BorrowedHandle<'_>,
+    other_process: Option<BorrowedHandle<'_>>,
+) -> io::Result<RawHandle> {
     let mut new_handle = INVALID_HANDLE_VALUE;
     let success = unsafe {
         let proc = GetCurrentProcess();
@@ -34,7 +37,7 @@ fn duplicate_handle_inner(handle: BorrowedHandle<'_>, other_process: Option<Borr
             DUPLICATE_SAME_ACCESS,
         ) != 0
     };
-    ok_or_ret_errno!(success => new_handle)
+    ok_or_ret_errno!(success => new_handle as _)
 }
 
 pub fn init_security_attributes() -> SECURITY_ATTRIBUTES {
