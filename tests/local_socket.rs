f9753ef0deed38f88be0b03ca02459c0cffd9897
Kotauskas <v.toncharov@gmail.com>
2024-01-28 11:26:57 +0300

    Splitting test for local sockets

--- tests/local_socket.rs
+++ tests/local_socket.rs
@@ -1,31 +1,50 @@
 // TODO test various error conditions
-// TODO test reunite in some shape or form
 
 mod no_server;
 mod stream;
 
 use crate::{local_socket::NameTypeSupport, tests::util::*};
 
-fn test_stream(nmspc: bool) -> TestResult {
+fn test_stream(split: bool, nmspc: bool) -> TestResult {
     use stream::*;
     testinit();
-    drive_server_and_multiple_clients(|s, n| server(s, n, nmspc), client)?;
+    let hcl = if split { handle_client_split as _ } else { handle_client_nosplit as _ };
+    let scl = |s, n| server(hcl, s, n, nmspc);
+    // I love the Rust typesystem
+    if split {
+        drive_server_and_multiple_clients(scl, client_split)?;
+    } else {
+        drive_server_and_multiple_clients(scl, client_nosplit)?;
+    }
     Ok(())
 }
 
-#[test]
-fn stream_file() -> TestResult {
-    if NameTypeSupport::query().paths_supported() {
-        test_stream(false)?;
-    }
-    Ok(())
+macro_rules! matrix {
+    (@querymethod true $e:expr) => { NameTypeSupport::namespace_supported($e) };
+    (@querymethod false $e:expr) => { NameTypeSupport::paths_supported($e) };
+    (@body $split:ident $nmspc:ident) => {{
+        if matrix!(@querymethod $nmspc NameTypeSupport::query()) {
+            test_stream(true, $nmspc)?;
+        }
+        Ok(())
+    }};
+    ($nm:ident false $nmspc:ident) => {
+        #[test]
+        fn $nm() -> TestResult { matrix!(@body false $nmspc) }
+    };
+    ($nm:ident true $nmspc:ident) => {
+        #[test]
+        #[cfg(not(windows))]
+        fn $nm() -> TestResult { matrix!(@body true $nmspc) }
+    };
+    ($($nm:ident $split:ident $nmspc:ident)+) => { $(matrix!($nm $split $nmspc);)+ };
 }
-#[test]
-fn stream_namespaced() -> TestResult {
-    if NameTypeSupport::query().namespace_supported() {
-        test_stream(true)?;
-    }
-    Ok(())
+
+matrix! {
+    stream_file_nosplit       false false
+    stream_file_split          true false
+    stream_namespaced_nosplit false  true
+    stream_namespaced_split    true  true
 }
 
 #[test]
--- tests/local_socket/stream.rs
+++ tests/local_socket/stream.rs
@@ -7,53 +7,103 @@ use std::{
     io::{BufRead, BufReader, Write},
     str,
     sync::{mpsc::Sender, Arc},
+    thread,
 };
 
 fn msg(server: bool, nts: bool) -> Box<str> {
     message(None, server, Some(['\n', '\0'][nts as usize]))
 }
 
-pub fn server(name_sender: Sender<Arc<str>>, num_clients: u32, namespaced: bool) -> TestResult {
+pub fn server(
+    handle_client: fn(LocalSocketStream) -> TestResult,
+    name_sender: Sender<Arc<str>>,
+    num_clients: u32,
+    namespaced: bool,
+) -> TestResult {
     let (name, listener) = listen_and_pick_name(&mut NameGen::new(make_id!(), namespaced), |nm| {
         LocalSocketListener::bind(nm)
     })?;
-
     let _ = name_sender.send(name);
+    listener
+        .incoming()
+        .take(num_clients.try_into().unwrap())
+        .try_for_each(|conn| handle_client(conn.context("accept failed")?))
+}
 
-    for _ in 0..num_clients {
-        let mut conn = listener.accept().context("accept failed").map(BufReader::new)?;
-        recv(&mut conn, msg(false, false), 0)?;
-        send(&mut conn, msg(true, false), 0)?;
-        recv(&mut conn, msg(false, true), 0)?;
-        send(&mut conn, msg(true, true), 0)?;
-    }
+pub fn handle_client_nosplit(conn: LocalSocketStream) -> TestResult {
+    let mut conn = BufReader::new(conn);
+    recv(&mut conn, &msg(false, false), 0)?;
+    send(conn.get_mut(), &msg(true, false), 0)?;
+    recv(&mut conn, &msg(false, true), 1)?;
+    send(conn.get_mut(), &msg(true, true), 1)
+}
+
+pub fn handle_client_split(conn: LocalSocketStream) -> TestResult {
+    let (recver, sender) = conn.split();
+
+    let recv = thread::spawn(move || {
+        let mut recver = BufReader::new(recver);
+        recv(&mut recver, &msg(true, false), 0)?;
+        recv(&mut recver, &msg(true, true), 1)?;
+        TestResult::<_>::Ok(recver.into_inner())
+    });
+    let send = thread::spawn(move || {
+        let mut sender = sender;
+        send(&mut sender, &msg(false, false), 0)?;
+        send(&mut sender, &msg(false, true), 1)?;
+        TestResult::<_>::Ok(sender)
+    });
+
+    let recver = recv.join().unwrap()?;
+    let sender = send.join().unwrap()?;
+    LocalSocketStream::reunite(recver, sender).context("reunite failed")?;
     Ok(())
 }
-pub fn client(name: &str) -> TestResult {
+
+pub fn client_nosplit(name: &str) -> TestResult {
     let mut conn =
         LocalSocketStream::connect(name).context("connect failed").map(BufReader::new)?;
-    send(&mut conn, msg(false, false), 0)?;
-    recv(&mut conn, msg(true, false), 0)?;
-    send(&mut conn, msg(false, true), 0)?;
-    recv(&mut conn, msg(true, true), 0)
+    send(conn.get_mut(), &msg(false, false), 0)?;
+    recv(&mut conn, &msg(true, false), 0)?;
+    send(conn.get_mut(), &msg(false, true), 1)?;
+    recv(&mut conn, &msg(true, true), 1)
 }
 
-fn recv(conn: &mut BufReader<LocalSocketStream>, exp: impl AsRef<str>, nr: u8) -> TestResult {
-    let exp_ = exp.as_ref();
-    let term = *exp_.as_bytes().last().unwrap();
+pub fn client_split(name: &str) -> TestResult {
+    let (recver, sender) = LocalSocketStream::connect(name).context("connect failed")?.split();
+
+    let recv = thread::spawn(move || {
+        let mut recver = BufReader::new(recver);
+        recv(&mut recver, &msg(false, false), 0)?;
+        recv(&mut recver, &msg(false, true), 1)?;
+        TestResult::<_>::Ok(recver.into_inner())
+    });
+    let send = thread::spawn(move || {
+        let mut sender = sender;
+        send(&mut sender, &msg(true, false), 0)?;
+        send(&mut sender, &msg(true, true), 1)?;
+        TestResult::<_>::Ok(sender)
+    });
+
+    let recver = recv.join().unwrap()?;
+    let sender = send.join().unwrap()?;
+    LocalSocketStream::reunite(recver, sender).context("reunite failed")?;
+    Ok(())
+}
+
+fn recv(conn: &mut dyn BufRead, exp: &str, nr: u8) -> TestResult {
+    let term = *exp.as_bytes().last().unwrap();
     let fs = ["first", "second"][nr as usize];
 
-    let mut buffer = Vec::with_capacity(exp_.len());
+    let mut buffer = Vec::with_capacity(exp.len());
     conn.read_until(term, &mut buffer).with_context(|| format!("{} receive failed", fs))?;
     ensure_eq!(
         str::from_utf8(&buffer).with_context(|| format!("{} receive wasn't valid UTF-8", fs))?,
-        exp_,
+        exp,
     );
     Ok(())
 }
-fn send(conn: &mut BufReader<LocalSocketStream>, msg: impl AsRef<str>, nr: u8) -> TestResult {
+fn send(conn: &mut dyn Write, msg: &str, nr: u8) -> TestResult {
     let fs = ["first", "second"][nr as usize];
-    conn.get_mut()
-        .write_all(msg.as_ref().as_bytes())
-        .with_context(|| format!("{} socket send failed", fs))
+    conn.write_all(msg.as_bytes()).with_context(|| format!("{} socket send failed", fs))
 }
