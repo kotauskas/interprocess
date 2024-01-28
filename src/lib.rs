4e28c8b8c38f0599517b703d757c22b41ffbc315
Kotauskas <v.toncharov@gmail.com>
2024-01-28 15:56:14 +0300

    Move lints to manifest

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,17 @@
 #![doc = include_str!("../README.md")]
-// TODO mailslots
+#![cfg_attr(feature = "doc_cfg", feature(doc_cfg))]
+mod platform_check;
+
 // TODO add OS-specific ext-traits
 // TODO un-mod.rs
-// TODO make doctests not no_run
 // TODO inspect panic points
-#![cfg_attr(feature = "doc_cfg", feature(doc_cfg))]
-#![deny(rust_2018_idioms)]
-#![warn(missing_docs)]
-#![allow(clippy::nonstandard_macro_braces)]
-#![forbid(unsafe_op_in_unsafe_fn)]
-
-#[cfg(any(not(any(windows, unix)), target_os = "emscripten"))]
-compile_error!(
-    "Your target operating system is not supported by interprocess – check if yours is in the list \
-of supported systems, and if not, please open an issue on the GitHub repository if you think that \
-it should be included"
-);
-
-#[cfg(not(any(target_pointer_width = "32", target_pointer_width = "64")))]
-compile_error!(
-    "Platforms with exotic pointer widths (neither 32-bit nor 64-bit) are not supported by \
-interprocess – if you think that your specific case needs to be accounted for, please open an \
-issue on the GitHub repository"
-);
 
 #[macro_use]
 mod macros;
 
+pub mod error;
 pub mod local_socket;
 pub mod unnamed_pipe;
-//pub mod shared_memory;
-
-pub mod error;
-
-mod try_clone;
-pub use try_clone::*;
-
-mod misc;
-pub(crate) use misc::*;
 
 /// Platform-specific functionality for various interprocess communication primitives.
 ///
@@ -56,6 +30,12 @@ pub mod os {
     #[cfg_attr(feature = "doc_cfg", doc(cfg(windows)))]
     pub mod windows;
 }
+
+mod try_clone;
+pub use try_clone::*;
+
+mod misc;
+pub(crate) use misc::*;
 
 #[cfg(test)]
 #[path = "../tests/index.rs"]
