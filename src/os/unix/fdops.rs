4a11cf03c2fe2ebccf7a4d729b18501f3153fecc
Kotauskas <v.toncharov@gmail.com>
2024-06-09 17:17:16 +0300

    Minor `FdOps` utilities

--- src/os/unix/fdops.rs
+++ src/os/unix/fdops.rs
@@ -1,6 +1,9 @@
 use super::{c_wrappers, unixprelude::*};
-use crate::{OrErrno, TryClone};
-use std::io::{self, prelude::*, IoSlice, IoSliceMut};
+use crate::{weaken_buf_init_mut, OrErrno, TryClone};
+use std::{
+	io::{self, prelude::*, IoSlice, IoSliceMut},
+	mem::MaybeUninit,
+};
 
 #[allow(clippy::as_conversions)]
 fn i2u(i: isize) -> usize {
@@ -9,26 +12,38 @@ fn i2u(i: isize) -> usize {
 
 #[repr(transparent)]
 pub(super) struct FdOps(pub(super) OwnedFd);
-impl Read for &FdOps {
-	fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+impl FdOps {
+	pub(super) fn read_uninit(
+		fd: BorrowedFd<'_>,
+		buf: &mut [MaybeUninit<u8>],
+	) -> io::Result<usize> {
 		let length_to_read = buf.len();
 		let bytes_read =
-			unsafe { libc::read(self.0.as_raw_fd(), buf.as_mut_ptr().cast(), length_to_read) };
+			unsafe { libc::read(fd.as_raw_fd(), buf.as_mut_ptr().cast(), length_to_read) };
 		(bytes_read >= 0).true_val_or_errno(i2u(bytes_read))
+	}
+	pub(super) fn write(fd: BorrowedFd<'_>, buf: &[u8]) -> io::Result<usize> {
+		let length_to_write = buf.len();
+		let bytes_written =
+			unsafe { libc::write(fd.as_raw_fd(), buf.as_ptr().cast(), length_to_write) };
+		(bytes_written >= 0).true_val_or_errno(i2u(bytes_written))
+	}
+}
+impl Read for &FdOps {
+	fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+		FdOps::read_uninit(self.as_fd(), weaken_buf_init_mut(buf))
 	}
 	fn read_vectored(&mut self, bufs: &mut [IoSliceMut<'_>]) -> io::Result<usize> {
 		let num_bufs = c_int::try_from(bufs.len()).unwrap_or(c_int::MAX);
-		let bytes_read = unsafe { libc::readv(self.0.as_raw_fd(), bufs.as_ptr().cast(), num_bufs) };
+		let bytes_read =
+			unsafe { libc::readv(self.0.as_raw_fd(), bufs.as_ptr().cast(), num_bufs) };
 		(bytes_read >= 0).true_val_or_errno(i2u(bytes_read))
 	}
 	// FUTURE can_vector
 }
 impl Write for &FdOps {
 	fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-		let length_to_write = buf.len();
-		let bytes_written =
-			unsafe { libc::write(self.0.as_raw_fd(), buf.as_ptr().cast(), length_to_write) };
-		(bytes_written >= 0).true_val_or_errno(i2u(bytes_written))
+		FdOps::write(self.as_fd(), buf)
 	}
 	fn write_vectored(&mut self, bufs: &[IoSlice<'_>]) -> io::Result<usize> {
 		let num_bufs = c_int::try_from(bufs.len()).unwrap_or(c_int::MAX);
