3e92926d7008e138b54279b87b2b0218dd8622fc
Kotauskas <v.toncharov@gmail.com>
2024-06-09 17:18:05 +0300

    Document lack of nonblocking mode assumption in local sockets

--- src/os/unix/uds_local_socket/tokio/listener.rs
+++ src/os/unix/uds_local_socket/tokio/listener.rs
@@ -23,9 +23,15 @@ impl traits::Listener for Listener {
 
 	fn from_options(options: ListenerOptions<'_>) -> io::Result<Self> {
 		options
-			.nonblocking(ListenerNonblockingMode::Neither)
+			.nonblocking(ListenerNonblockingMode::Both)
 			.create_sync_as::<SyncListener>()
-			.and_then(Self::try_from)
+			.and_then(|mut sync| {
+				let reclaim = sync.reclaim.take();
+				Ok(Self {
+					listener: UnixListener::from_std(sync.into())?,
+					reclaim,
+				})
+			})
 	}
 	async fn accept(&self) -> io::Result<Stream> {
 		let inner = self.listener.accept().await?.0;
@@ -37,10 +43,13 @@ impl traits::Listener for Listener {
 	}
 }
 
+/// Does not assume that the sync `Listener` is in nonblocking mode, setting it to
+/// `ListenerNonblockingMode::Both` automatically.
+// TODO(3.0.0) remove handholding and assume nonblocking
 impl TryFrom<SyncListener> for Listener {
 	type Error = io::Error;
 	fn try_from(mut sync: SyncListener) -> io::Result<Self> {
-		sync.set_nonblocking(ListenerNonblockingMode::Accept)?;
+		sync.set_nonblocking(ListenerNonblockingMode::Both)?;
 		let reclaim = sync.reclaim.take();
 		Ok(Self {
 			listener: UnixListener::from_std(sync.into())?,
@@ -72,6 +81,8 @@ impl TryFrom<Listener> for OwnedFd {
 		})
 	}
 }
+/// Does not assume that the listener is in nonblocking mode, setting it to
+/// `ListenerNonblockingMode::Both` automatically.
 impl TryFrom<OwnedFd> for Listener {
 	type Error = io::Error;
 	fn try_from(fd: OwnedFd) -> io::Result<Self> {
