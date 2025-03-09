49b054472ef908f92163fc0140498863d6fc05a4
Goat <v.toncharov@gmail.com>
2025-03-09 21:54:31 +0300

    Elide more lifetimes

--- src/os/windows/named_pipe/listener/incoming.rs
+++ src/os/windows/named_pipe/listener/incoming.rs
@@ -9,7 +9,7 @@ use {
 /// This iterator is created by the [`.incoming()`](PipeListener::incoming) method on
 /// [`PipeListener`]. See its documentation for more.
 pub struct Incoming<'a, Rm: PipeModeTag, Sm: PipeModeTag>(pub(super) &'a PipeListener<Rm, Sm>);
-impl<'a, Rm: PipeModeTag, Sm: PipeModeTag> Iterator for Incoming<'a, Rm, Sm> {
+impl<Rm: PipeModeTag, Sm: PipeModeTag> Iterator for Incoming<'_, Rm, Sm> {
     type Item = io::Result<PipeStream<Rm, Sm>>;
     #[inline]
     fn next(&mut self) -> Option<Self::Item> { Some(self.0.accept()) }
